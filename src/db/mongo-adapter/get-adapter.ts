f5190bd46469f118159c3c61edeccf7b46705ad4
Whytecrowe <dor7an@gmail.com>
2024-08-19 15:44:48 -0700

    fix getMongoAdapter function for correct version key

--- src/db/mongo-adapter/get-adapter.ts
+++ src/db/mongo-adapter/get-adapter.ts
@@ -17,57 +17,35 @@ export const getMongoAdapter = async ({
   logger ?: TLogger;
   contractsVersion ?: string;
 } = {}) : Promise<MongoDBAdapter> => {
-  const checkParams = {
+  logger = !logger ? getLogger() : logger;
+
+  const params = {
+    logger,
     dbUri: process.env.MONGO_DB_URI
       ? process.env.MONGO_DB_URI
       : DEFAULT_MONGO_URI,
     dbName: process.env.MONGO_DB_NAME
       ? process.env.MONGO_DB_NAME
       : DEFAULT_MONGO_DB_NAME,
-  };
-
-  logger = !logger ? getLogger() : logger;
-
-  const params = {
-    logger,
     clientOpts: process.env.MONGO_DB_CLIENT_OPTS
       ? JSON.parse(process.env.MONGO_DB_CLIENT_OPTS)
       : undefined,
-    version: process.env.MONGO_DB_VERSION
+    dbVersion: process.env.MONGO_DB_VERSION
       ? process.env.MONGO_DB_VERSION
       : undefined,
     archive: process.env.ARCHIVE_PREVIOUS_DB_VERSION === "true",
     contractsVersion,
   };
 
-  let createNew = false;
-  if (mongoAdapter) {
-    Object.values(checkParams).forEach(
-      ([key, value]) => {
-        if (key === "version") key = "curVersion";
-
-        // if the existing adapter was created with different options than the currently needed one
-        // we create a new one and overwrite
-        if (JSON.stringify(mongoAdapter?.[key]) !== JSON.stringify(value)) {
-          createNew = true;
-          return;
-        }
-      }
-    );
-  } else {
-    createNew = true;
-  }
-
-  if (createNew) {
+  if (!mongoAdapter) {
     logger.debug("Creating new MongoDBAdapter instance");
     mongoAdapter = new MongoDBAdapter({
-      ...checkParams,
       ...params,
     });
-    await mongoAdapter.initialize(params.version);
+    await mongoAdapter.initialize();
   } else {
     logger.debug("Returning existing MongoDBAdapter instance");
   }
 
-  return mongoAdapter as MongoDBAdapter;
+  return mongoAdapter ;
 };
